10c1a2dca996c547f68c03f8113d31c170a8aa24
nocheatplus <arzuariferen@gmail.com>
2023-06-05 18:06:37 +0300

    Hesap makinesi by Xalieren.cpp

---
+++ Hesap makinesi by Xalieren.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int sayi1, sayi2;
+	char islem;
+	cout << "Hesap makinesine hosgeldiniz!" << endl << "Birinci sayiyi giriniz: " << endl;
+	cin >> sayi1;
+	cout << "Ikinci sayiyi giriniz: " << endl;
+	cin >> sayi2;
+	cout << "T: Toplama" << endl;
+	cout << "C: Cikartma" << endl;
+	cout << "P: Carpma" << endl;
+	cout << "B: Bolme" << endl;
+	cout << "Islem seciniz: " << endl;
+	cin >> islem;
+
+	if (islem == 'T')
+	{
+		cout << "Sonuc: " << sayi1 + sayi2 << endl;
+		cout << "by Xalieren" << endl;
+	}
+
+	else if (islem == 'C')
+	{
+		cout << "Sonuc: " << sayi1 - sayi2 << endl;
+		cout << "by Xalieren" << endl;
+	}
+
+	else if (islem=='P')
+	{
+		cout << "Sonuc: " << sayi1 * sayi2 << endl;
+		cout << "by Xalieren" << endl;
+	}
+
+	else if (islem == 'B')
+	{
+		cout << "Sonuc: " << sayi1 / sayi2 << endl;
+		cout << "by Xalieren" << endl;
+	}
+
+	else
+	{
+		cout << "Lutfen programi yeniden baslatin!" << endl;
+		cout << "by Xalieren" << endl;
+	}
+	system("PAUSE");
+	return 0;
+}
